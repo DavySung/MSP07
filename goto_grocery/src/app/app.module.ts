54e542c1503fda5c8996082a05c8db4a0001ae5a
103062579 <88230766+103062579@users.noreply.github.com>
2022-09-06 22:51:40 +1000

    setup-of-components

--- goto_grocery/src/app/app.module.ts
+++ goto_grocery/src/app/app.module.ts
@@ -3,10 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { MainLayoutComponent } from './components/layouts/main-layout/main-layout.component';
+import { MainMenuComponent } from './components/shared/main-menu/main-menu.component';
+import { InventoryComponent } from './components/pages/inventory/inventory.component';
+import { MembersComponent } from './components/pages/members/members.component';
+import { SalesComponent } from './components/pages/sales/sales.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MainLayoutComponent,
+    MainMenuComponent,
+    InventoryComponent,
+    MembersComponent,
+    SalesComponent
   ],
   imports: [
     BrowserModule,
