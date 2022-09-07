71a424453cb3f5a8aa5e1030245d3455a3ef3a84
Rob Findlay <rjf1112@gmail.com>
2022-09-07 22:57:30 +1000

    (not tested) create models for product/price and transaction

---
+++ node/migrations/20220907123426-create-product.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      productCode: {
+        type: Sequelize.STRING
+      },
+      productName: {
+        type: Sequelize.STRING
+      },
+      productDesc: {
+        type: Sequelize.STRING
+      },
+      createdDate: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Products');
+  }
+};
---
+++ node/migrations/20220907124741-create-transaction.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Transactions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      customerNumber: {
+        type: Sequelize.STRING
+      },
+      productCode: {
+        type: Sequelize.STRING
+      },
+      transactionDate: {
+        type: Sequelize.DATE
+      },
+      productPriceID: {
+        type: Sequelize.INTEGER
+      },
+      orderID: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Transactions');
+  }
+};
