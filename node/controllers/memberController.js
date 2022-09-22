321e707b2eb52feb519fd5410436bf51b22779c8
Rob Findlay <rjf1112@gmail.com>
2022-09-22 05:07:32 +0000

    Basic controller setup

---
+++ node/controllers/memberController.js
@@ -0,0 +1,14 @@
+const memberService = require('../services/memberService.js')
+
+exports.getMembers = async(req, res) => {
+    try {
+        return res.status(200).send(await memberService.GetMembersAsync());
+    } catch (error) {
+        return res.status(500).send({
+            status: false,
+            error: error.message,
+        });
+    }
+}
+
+exports.deleteMember = 
