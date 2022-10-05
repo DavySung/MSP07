d8ef20f311868fffbbce11aaaf36fc979a2e6e45
JDManser <manserjd@gmail.com>
2022-10-05 21:00:08 +1100

    Updated model

--- node/seeders/20220921074840-demo-member.js
+++ node/seeders/20220921074840-demo-member.js
@@ -3,11 +3,13 @@
 module.exports = {
   async up (queryInterface, Sequelize) {
     
-     await queryInterface.bulkInsert('Members', [{
+     await queryInterface.bulkInsert('Members', 
+     [
+      {
         customerNumber: '12345678',
-        firstName: 'Jeff',
-        lastName: 'Goldbloom',
-        email: 'jeff.goldbloom@jurassicpark.com',
+        firstName: 'Ian',
+        lastName: 'Malcolm',
+        email: 'dr.malcolm@jurassicpark.com',
         phone: '0456889576',
         addressFirstLine: '1 Dinosaur Way',
         addressSuburb: 'Tropical paradise',
@@ -16,7 +18,36 @@ module.exports = {
         accountActiveIndicator: 'True',
         createdAt: new Date(),
         updatedAt: new Date()
-     }])
+     },
+     {
+      customerNumber: '12345610',
+      firstName: 'Ellie',
+      lastName: 'Sadler',
+      email: 'triceretops.poop@jurpark.com',
+      phone: '0456889876',
+      addressFirstLine: '100 Raptor Pen',
+      addressSuburb: 'Tropical paradise',
+      addressState: 'VIC',
+      addressPostcode: '3000',
+      accountActiveIndicator: 'True',
+      createdAt: new Date(),
+      updatedAt: new Date()
+   },
+   {
+    customerNumber: '12345679',
+    firstName: 'Dennis',
+    lastName: 'Nedry',
+    email: 'shaving.cream@dilophosaurus.com',
+    phone: '0456899576',
+    addressFirstLine: '1 Deadmans Drive',
+    addressSuburb: 'Tropical paradise',
+    addressState: 'VIC',
+    addressPostcode: '3000',
+    accountActiveIndicator: 'True',
+    createdAt: new Date(),
+    updatedAt: new Date()
+ }
+    ])
   },
 
   async down (queryInterface, Sequelize) {
